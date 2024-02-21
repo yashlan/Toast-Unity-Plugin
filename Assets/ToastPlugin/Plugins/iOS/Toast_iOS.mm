d680056fc95ade86a3a8de774ac80cc57132e475
yashlan <yashlan007@gmail.com>
2024-02-21 21:11:56 +0700

    Update Toast_iOS.mm

--- Assets/ToastPlugin/Plugins/iOS/Toast_iOS.mm
+++ Assets/ToastPlugin/Plugins/iOS/Toast_iOS.mm
@@ -2,12 +2,16 @@
 
 @interface Toast_iOS : UIViewController
 
+@property (nonatomic, strong) UILabel *toastLabel;
 + (instancetype)sharedInstance;
 - (void)showToast:(NSString *)message withDuration:(float)duration;
+- (void)cancelToast;
 
 @end
 
 @implementation Toast_iOS
+
+@synthesize toastLabel;
 
 + (instancetype) sharedInstance {
     static Toast_iOS *sharedInstance = nil;
@@ -29,23 +33,42 @@
     lab.alpha = 1.0;
     lab.layer.cornerRadius = 10;
     lab.clipsToBounds = YES;
+    lab.numberOfLines = 0; // Allow multiline
     
+    CGFloat bottomMargin = 100;
+
     CGSize textSize = [message sizeWithAttributes:@{NSFontAttributeName: lab.font}];
     CGFloat labelWidth = textSize.width + 20;
-    
+        
     CGFloat maxWidth = rootViewController.view.frame.size.width - 40;
     labelWidth = MIN(labelWidth, maxWidth);
     
-    lab.frame = CGRectMake((rootViewController.view.frame.size.width - labelWidth) / 2, rootViewController.view.frame.size.height - 100, labelWidth, 50);
+    CGSize maxSize = CGSizeMake(rootViewController.view.frame.size.width - 40, CGFLOAT_MAX);
+    lab.frame = CGRectMake(20, rootViewController.view.frame.size.height - bottomMargin, maxSize.width, 0);
+
+    CGSize labelSize = [lab sizeThatFits:maxSize];
+    lab.frame = CGRectMake((rootViewController.view.frame.size.width - labelWidth) / 2,
+                           rootViewController.view.frame.size.height - labelSize.height - bottomMargin,
+                           labelWidth,
+                           labelSize.height + 20);
     
     [rootViewController.view addSubview:lab];
-
     
-    [UIView animateWithDuration:duration delay:0.1 options:UIViewAnimationOptionCurveEaseOut animations:^{
+    self.toastLabel = lab;
+    
+    [UIView animateWithDuration:duration delay:duration options:UIViewAnimationOptionCurveEaseOut animations:^{
         lab.alpha = 0.0;
     } completion:^(BOOL isCompleted) {
         [lab removeFromSuperview];
     }];
+}
+
+- (void) cancelToast {
+    if (self.toastLabel) {
+        [self.toastLabel.layer removeAllAnimations];
+        [self.toastLabel removeFromSuperview];
+        self.toastLabel = nil;
+    }
 }
 
 @end
@@ -56,5 +79,10 @@ extern "C" {
     {
         NSString *messageString = [NSString stringWithUTF8String:message];
         [[Toast_iOS sharedInstance] showToast:messageString withDuration:duration];
+    }
+
+    void cancelToast()
+    {
+        [[Toast_iOS sharedInstance] cancelToast];
     }
 }
